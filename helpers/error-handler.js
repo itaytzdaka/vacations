d468b1abb1a46c75f3826e2b44adf6e86159966b
itaytzdaka <itaytzdaka1@gmail.com>
2024-06-09 15:25:17 +0300

    turn on error details in production

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -1,8 +1,8 @@
 function getError(err) {
 
-    if (process.env.NODE_ENV === "production") {
-        return "Some error occurred, please try again later.";
-    }
+    // if (process.env.NODE_ENV === "production") {
+    //     return "Some error occurred, please try again later.";
+    // }
 
     return err.message;
 }
